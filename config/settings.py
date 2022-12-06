f08b58bcec222fa0833f1625cc32758473a3d686
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2022-12-06 12:06:27 +0300

    first commit

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "markdownify.apps.MarkdownifyConfig",
+    "markdownify",
     "social_django",
     "mainapp",
     "authapp",
